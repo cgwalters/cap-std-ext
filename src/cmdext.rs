0d0d566811395a5fd76fdf092cb9c7607977a8a0
Colin Walters <walters@verbum.org>
2024-07-26 17:34:43 -0400

    cmdext: Add helpers to directly take Dir/File
    In some cases it is maximally efficient to share file descriptors
    with `Arc`, but in many cases that's not possible. These convenience
    helpers allow directly passing owned descriptors.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- src/cmdext.rs
+++ src/cmdext.rs
@@ -10,6 +10,7 @@ use cap_std::io_lifetimes;
 use cap_tempfile::cap_std;
 use io_lifetimes::OwnedFd;
 use rustix::fd::{AsFd, FromRawFd, IntoRawFd};
+use std::fs::File;
 use std::os::unix::process::CommandExt;
 use std::sync::Arc;
 
@@ -17,8 +18,21 @@ use std::sync::Arc;
 ///
 /// [`cap_std::fs::Dir`]: https://docs.rs/cap-std/latest/cap_std/fs/struct.Dir.html
 pub trait CapStdExtCommandExt {
-    /// Pass a file descriptor into the target process.
+    /// Pass a file descriptor into the target process, which will see it as
+    /// the provided file descriptor number. You must
+    /// to choose e.g. file descriptor 3 or above unless you're very specifically
+    /// intending to replace one of the standard I/O streams.
     fn take_fd_n(&mut self, fd: Arc<OwnedFd>, target: i32) -> &mut Self;
+
+    /// Pass a [`Dir`] to the child.
+    fn take_dirfd_n(&mut self, fd: Dir, target: i32) -> &mut Self {
+        self.take_fd_n(Arc::new(fd.into()), target)
+    }
+
+    /// Pass a [`File`] to the child.
+    fn take_file_n(&mut self, fd: File, target: i32) -> &mut Self {
+        self.take_fd_n(Arc::new(fd.into()), target)
+    }
 
     /// Use the given directory as the current working directory for the process.
     fn cwd_dir(&mut self, dir: Dir) -> &mut Self;
